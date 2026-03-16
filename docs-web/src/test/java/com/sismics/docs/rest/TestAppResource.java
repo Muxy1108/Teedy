80c80a0d61a7b7f0b22e9f561d60c114f010f153
Muxy1108 <2175968350@qq.com>
2026-03-16 15:30:19 +0800

    add guest login button

--- docs-web/src/test/java/com/sismics/docs/rest/TestAppResource.java
+++ docs-web/src/test/java/com/sismics/docs/rest/TestAppResource.java
@@ -57,7 +57,7 @@ public class TestAppResource extends BaseJerseyTest {
         Long totalMemory = json.getJsonNumber("total_memory").longValue();
         Assert.assertTrue(totalMemory > 0 && totalMemory > freeMemory);
         Assert.assertEquals(0, json.getJsonNumber("queued_tasks").intValue());
-        Assert.assertFalse(json.getBoolean("guest_login"));
+        Assert.assertTrue(json.getBoolean("guest_login"));
         Assert.assertFalse(json.getBoolean("ocr_enabled"));
         Assert.assertEquals("eng", json.getString("default_language"));
         Assert.assertTrue(json.containsKey("global_storage_current"));
@@ -139,20 +139,9 @@ public class TestAppResource extends BaseJerseyTest {
     public void testGuestLogin() {
         // Login admin
         String adminToken = adminToken();
+        Response response;
 
-        // Try to login as guest
-        Response response = target().path("/user/login").request()
-                .post(Entity.form(new Form()
-                        .param("username", "guest")));
-        Assert.assertEquals(Status.FORBIDDEN.getStatusCode(), response.getStatus());
-
-        // Enable guest login
-        target().path("/app/guest_login").request()
-                .cookie(TokenBasedSecurityFilter.COOKIE_NAME, adminToken)
-                .post(Entity.form(new Form()
-                        .param("enabled", "true")), JsonObject.class);
-
-        // Login as guest
+        // Guest login is enabled by default
         String guestToken = clientUtil.login("guest", "", false);
 
         // Guest cannot delete himself
@@ -196,7 +185,7 @@ public class TestAppResource extends BaseJerseyTest {
                 .cookie(TokenBasedSecurityFilter.COOKIE_NAME, guestToken)
                 .get(JsonObject.class);
 
-        // Disable guest login (clean up state)
+        // Disable guest login (clean up state for tests that expect the toggle to work both ways)
         target().path("/app/guest_login").request()
                 .cookie(TokenBasedSecurityFilter.COOKIE_NAME, adminToken)
                 .post(Entity.form(new Form()
